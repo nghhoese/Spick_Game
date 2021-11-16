3b2754ff7ac90488ec2b16dc11c960f116141365
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-11-16 10:59:06 +0100

    mouse position

--- Spick_Game/Spick_Game/Spick_Game.cpp
+++ Spick_Game/Spick_Game/Spick_Game.cpp
@@ -4,13 +4,16 @@
 #include <iostream>
 #include "API_Headers/Importation.hpp"
 #include "API_Headers/Point.hpp"
+#include "API_Headers/KeyCode.hpp"
 
 int main()
 {
     spic::Importation* k = new spic::Importation();
+    spic::KeyCode c = spic::KeyCode::W;
     
     while (true) {
         spic::Point point = k->MousePosition();
         std::cout << point.x << point.y << std::endl;
+        //k->GetKeyDown(c);
     }
 }
