5930704b1ba8e9c14571e674d2e6abd44fa17307
Nick Avans <nghhoese@avans.nl>
2021-11-16 21:29:59 +0100

    New implementation code

--- Spick_Game/Spick_Game/Spick_Game.cpp
+++ Spick_Game/Spick_Game/Spick_Game.cpp
@@ -9,22 +9,35 @@
 int main()
 {
     //spic::Scene* scene = new spic::Scene("test");
-    std::unique_ptr<Player> player = std::make_unique<Player>();
 
-    std::unique_ptr<spic::GameObject> gameObject = std::make_unique<spic::GameObject>("lol");
+
+  
+
+    std::shared_ptr<spic::GameObject> gameObject = std::make_shared<spic::GameObject>("lol");
 
     std::shared_ptr<spic::Scene> scene = std::make_shared<spic::Scene>("lol");
-
+    scene->AddGameObject(gameObject);
     gameObject->SetScene(scene);
-
-    spic::Sprite* sprite = new spic::Sprite();
+    std::shared_ptr < spic::Sprite> sprite = std::make_shared<spic::Sprite>();
+    gameObject->AddComponent(sprite);
     sprite->SetGameObject(gameObject.get());
+    sprite->SetSprite("C:\\Users\\Gebruiker\\Downloads\\player_sub-machinegun.bmp");
+    scene->Render();
+    
 
     
-    while (true) {
-        player->checkKeys();
-        player->checkMouseButtons();
-        player->checkMousePosition();
 
+
+
+   
+    while (true) {
+        
     }
+
+
+
+
+
+
+
 }
