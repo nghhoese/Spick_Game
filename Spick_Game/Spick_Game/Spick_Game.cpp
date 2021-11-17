0fc5b789c70c138faf9d5303b9dc653200fc5ff2
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-11-17 10:32:27 +0100

    assets in game

--- Spick_Game/Spick_Game/Spick_Game.cpp
+++ Spick_Game/Spick_Game/Spick_Game.cpp
@@ -8,36 +8,18 @@
 
 int main()
 {
-    //spic::Scene* scene = new spic::Scene("test");
-
-
-  
-
     std::shared_ptr<spic::GameObject> gameObject = std::make_shared<spic::GameObject>("lol");
-
     std::shared_ptr<spic::Scene> scene = std::make_shared<spic::Scene>("lol");
     scene->AddGameObject(gameObject);
     gameObject->SetScene(scene);
-    std::shared_ptr < spic::Sprite> sprite = std::make_shared<spic::Sprite>();
+    std::shared_ptr<spic::Sprite> sprite = std::make_shared<spic::Sprite>();
     gameObject->AddComponent(sprite);
     sprite->SetGameObject(gameObject.get());
-    sprite->SetSprite("C:\\Users\\Gebruiker\\Downloads\\player_sub-machinegun.bmp");
+    sprite->SetSprite("assets/player_sub-machinegun.bmp");
     scene->Render();
     
-
-    
-
-
-
-   
     while (true) {
         
     }
-
-
-
-
-
-
 
 }
