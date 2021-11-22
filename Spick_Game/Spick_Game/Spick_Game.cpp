4c252b8111b3b6fda1a77a39c3a4318a35885dab
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-11-22 15:56:14 +0100

    begin camera

--- Spick_Game/Spick_Game/Spick_Game.cpp
+++ Spick_Game/Spick_Game/Spick_Game.cpp
@@ -4,10 +4,12 @@
 #define SDL_MAIN_HANDLED
 #include "API_Headers/Scene.hpp"
 #include "API_Headers/Engine.hpp"
+#include "API_Headers/Camera.hpp"
 #include "API_Headers/Sprite.hpp"
 #include "API_Headers/Transform.hpp"
 #include "Player.hpp"
 #include "Level.hpp"
+#include "API_Headers/Rectangle.hpp"
 
 namespace fs = std::filesystem;
 
@@ -18,6 +20,15 @@ int main()
     const std::string& path = "assets/levels/level1.json";
     std::shared_ptr<Level> level = std::make_shared<Level>();
     level->BuildLevel(scene, path);
+
+    spic::Camera* camera = new spic::Camera("mainCamera");
+    camera->setAspectWidth(800);
+    camera->setAspectHeight(640);
+    camera->setX(0);
+    camera->setY(0);
+    camera->createRectangle();
+    spic::Rectangle rectangle = camera->getRectangle();
+
 
     spic::Engine* engine = new spic::Engine();
     engine->AddScene(scene);
