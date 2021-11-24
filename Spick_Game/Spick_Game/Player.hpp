4665d668ea728044c86788392e9f7c14269bd37a
RensAspersAvans <rca.aspers@student.avans.nl>
2021-11-24 14:17:32 +0100

    Revert "merge"
    This reverts commit 10da761884eca56066f77a2e9dfff989f5766877, reversing
    changes made to 5c94558f35d6e266ecb607d4343cd17a923af139.

--- Spick_Game/Spick_Game/Player.hpp
+++ Spick_Game/Spick_Game/Player.hpp
@@ -6,26 +6,23 @@
 #include "API_Headers/BehaviourScript.hpp"
 #include "API_Headers/Transform.hpp"
 #include "API_Headers/GameObject.hpp"
-#include <API_Headers/Rectangle.hpp>
-#include <API_Headers/Scene.hpp>
-#include <API_Headers/Camera.hpp>
 
 class Player : public spic::BehaviourScript {
 private:
 	int healthpoints;
 	int ammo;
 public:
-    Player();
 	double x;
 	double y;
 	const void checkMouseButtons();
 	const void checkKeys();
 	const spic::Point checkMousePosition();
-    void OnAwake();
-    void OnStart();
+   virtual void OnAwake();
+   void OnStart();
     void OnUpdate();
     void OnRender();
-    void OnTriggerEnter2D(const Collider& collider);
+   void OnTriggerEnter2D(const Collider& collider);
+   Player();
     /**
      * @brief Sent when another object leaves a trigger collider
      *        attached to this object (2D physics only).
@@ -36,5 +33,5 @@ public:
      * @brief Sent each frame where another object is within a trigger
      *        collider attached to this object (2D physics only).
      */
-    void OnTriggerStay2D(const Collider& collider);
+   void OnTriggerStay2D(const Collider& collider);
 };
