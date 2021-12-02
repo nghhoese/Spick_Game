67b2c616de622cf18bab4967195a9e3ad7c81ff1
Bas van Pelt <49717976+sgvanpelt@users.noreply.github.com>
2021-12-02 11:44:41 +0100

    cheatsmenu fix

--- Spick_Game/Spick_Game/Scenes/CheatsMenuBuilder.cpp
+++ Spick_Game/Spick_Game/Scenes/CheatsMenuBuilder.cpp
@@ -3,27 +3,37 @@
 std::shared_ptr<spic::Scene> CheatsMenuBuilder::BuildScene(spic::Engine* engine) {
     std::shared_ptr<spic::Scene> scene = std::make_shared<spic::Scene>("CheatsMenu");
 
-    buttonBackToLevel = std::make_shared<spic::Button>("button", 295,400, 72, 180, "StartGameScript");
+    buttonBackToLevel = std::make_shared<spic::Button>("button", 10,400, 75, 150, "StartGameScript");
     scene->AddGameObject(buttonBackToLevel);
 
     spic::Color textColor = spic::Color(1.0, 1.0, 1.0, 1.0);
-    std::shared_ptr<spic::Text> text = std::make_shared<spic::Text>("Cheats Menu", "Capsmall", 60, textColor, 10, 50);
-    scene->AddGameObject(text);
+    std::shared_ptr<spic::Text> HeaderText = std::make_shared<spic::Text>("Cheats Menu", "Capsmall", 60, textColor, 10, 50);
+    scene->AddGameObject(HeaderText);
 
-    std::shared_ptr<spic::Text> text2 = std::make_shared<spic::Text>("Up Movement Speed: Arrow Key Up", "Capsmall", 40, textColor, 10, 150);
-    scene->AddGameObject(text2);
+    std::shared_ptr<spic::Text> UpMovementSpeedText = std::make_shared<spic::Text>("Up Movement Speed:", "Capsmall", 40, textColor, 10, 150);
+    scene->AddGameObject(UpMovementSpeedText);
+    std::shared_ptr<spic::Text> MovementSpeedButtonText = std::make_shared<spic::Text>("Arrow Key Up", "Capsmall", 40, textColor, 500, 150);
+    scene->AddGameObject(MovementSpeedButtonText);
 
-    std::shared_ptr<spic::Text> text3 = std::make_shared<spic::Text>("Lower Movement Speed: Arrow Key Down", "Capsmall", 40, textColor, 10, 200);
-    scene->AddGameObject(text3);
+    std::shared_ptr<spic::Text> LowerMovementSpeedText = std::make_shared<spic::Text>("Lower Movement Speed:", "Capsmall", 40, textColor, 10, 200);
+    scene->AddGameObject(LowerMovementSpeedText);
+    std::shared_ptr<spic::Text> LowerMovementSpeedButtonText = std::make_shared<spic::Text>("Arrow Key Down", "Capsmall", 40, textColor, 500, 200);
+    scene->AddGameObject(LowerMovementSpeedButtonText);
 
-    std::shared_ptr<spic::Text> text4 = std::make_shared<spic::Text>("Enable Damageless: L", "Capsmall", 40, textColor, 10, 250);
-    scene->AddGameObject(text4);
+    std::shared_ptr<spic::Text> DamagelessText = std::make_shared<spic::Text>("Enable Damageless:", "Capsmall", 40, textColor, 10, 250);
+    scene->AddGameObject(DamagelessText);
+    std::shared_ptr<spic::Text> DamagelessButtonText = std::make_shared<spic::Text>("L", "Capsmall", 40, textColor, 500, 250);
+    scene->AddGameObject(DamagelessButtonText);
 
-    std::shared_ptr<spic::Text> text5 = std::make_shared<spic::Text>("Enable Undetectability: U", "Capsmall", 40, textColor, 10, 300);
-    scene->AddGameObject(text5);
+    std::shared_ptr<spic::Text> UndectabilityText = std::make_shared<spic::Text>("Enable Undetectability", "Capsmall", 40, textColor, 10, 300);
+    scene->AddGameObject(UndectabilityText);
+    std::shared_ptr<spic::Text> UndectabilityButtonText = std::make_shared<spic::Text>("U", "Capsmall", 40, textColor, 500, 300);
+    scene->AddGameObject(UndectabilityButtonText);
 
-    std::shared_ptr<spic::Text> text6 = std::make_shared<spic::Text>("Instakill the player: Y", "Capsmall", 40, textColor, 10, 350);
-    scene->AddGameObject(text6);
+    std::shared_ptr<spic::Text> InstakillText = std::make_shared<spic::Text>("Instakill the player:", "Capsmall", 40, textColor, 10, 350);
+    scene->AddGameObject(InstakillText);
+    std::shared_ptr<spic::Text> InstakillButtonText = std::make_shared<spic::Text>("Y", "Capsmall", 40, textColor, 500, 350);
+    scene->AddGameObject(InstakillButtonText);
 
     return scene;
     
