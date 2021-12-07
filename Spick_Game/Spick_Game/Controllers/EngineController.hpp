15dd4767b744d32061b7a720998560ed8d3a7f58
Bas van Pelt <49717976+sgvanpelt@users.noreply.github.com>
2021-12-07 14:30:00 +0100

    add method

--- Spick_Game/Spick_Game/Controllers/EngineController.hpp
+++ Spick_Game/Spick_Game/Controllers/EngineController.hpp
@@ -17,5 +17,6 @@ public:
 	void AddScene(std::shared_ptr<spic::Scene> scene);
 	void CreateNewWindow(const std::string& windowName);
 	void StartGameLoop();
+	std::shared_ptr<spic::Scene> GetSceneByName(const std::string& sceneName);
 	std::pair<std::vector<std::pair<int, std::vector<std::vector<int>>>>, std::vector<std::vector<std::pair<std::string, std::any>>>> GetLevel(const std::filesystem::path& path);
 };
