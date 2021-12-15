9849a9fb14e56ac69357731a2ea28c85b8afe815
Bas van Pelt <49717976+sgvanpelt@users.noreply.github.com>
2021-12-15 12:48:17 +0100

    fix

--- Spick_Game/Spick_Game/Scenes/CheatsMenuBuilder.cpp
+++ Spick_Game/Spick_Game/Scenes/CheatsMenuBuilder.cpp
@@ -13,9 +13,9 @@ std::shared_ptr<spic::Scene> CheatsMenuBuilder::BuildScene() {
     std::shared_ptr<spic::Text> head = std::make_shared<spic::Text>("Cheat Controls", "Capsmall", 60, textColor, 32, 120);
     std::shared_ptr<spic::Text> m1 = std::make_shared<spic::Text>("ONLY USABLE WHEN CHEATS ARE ENABLED", "Segment16CBold", 38, textColor, 38, 190);
     std::shared_ptr<spic::Text> c1 = std::make_shared<spic::Text>("ENABLE CHEATS          | C-KEY", "Segment16CBold", 38, textColor, 38, 230);
-    std::shared_ptr<spic::Text> c2 = std::make_shared<spic::Text>("DISABLE COLLISION      | F-KEY", "Segment16CBold", 38, textColor, 38, 270);
-    std::shared_ptr<spic::Text> c3 = std::make_shared<spic::Text>("ENABLE DAMAGELESS      | L-KEY", "Segment16CBold", 38, textColor, 38, 310);
-    std::shared_ptr<spic::Text> c4 = std::make_shared<spic::Text>("ENABLE OBJECTS HITBOX  | O-KEY", "Segment16CBold", 38, textColor, 38, 350);
+    std::shared_ptr<spic::Text> c3 = std::make_shared<spic::Text>("ENABLE DAMAGELESS      | L-KEY", "Segment16CBold", 38, textColor, 38, 270);
+    std::shared_ptr<spic::Text> c4 = std::make_shared<spic::Text>("ENABLE OBJECTS HITBOX  | O-KEY", "Segment16CBold", 38, textColor, 38, 310);
+    std::shared_ptr<spic::Text> c2 = std::make_shared<spic::Text>("DISABLE COLLISION      | Q-KEY", "Segment16CBold", 38, textColor, 38, 350);
     std::shared_ptr<spic::Text> c5 = std::make_shared<spic::Text>("SKIP LEVEL             | T-KEY", "Segment16CBold", 38, textColor, 38, 390);
     std::shared_ptr<spic::Text> c6 = std::make_shared<spic::Text>("ENABLE UNDETECTABILITY | U-KEY", "Segment16CBold", 38, textColor, 38, 430);
     std::shared_ptr<spic::Text> c7 = std::make_shared<spic::Text>("INSTAKILL PLAYER       | Y-KEY", "Segment16CBold", 38, textColor, 38, 470);
