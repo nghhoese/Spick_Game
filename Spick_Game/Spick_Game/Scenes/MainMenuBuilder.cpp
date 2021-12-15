8ce12561887b8e30d6c853b6bcef3e21b88fd7c0
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-12-15 19:57:55 +0100

    main menu schaalbaar

--- Spick_Game/Spick_Game/Scenes/MainMenuBuilder.cpp
+++ Spick_Game/Spick_Game/Scenes/MainMenuBuilder.cpp
@@ -13,32 +13,32 @@ std::shared_ptr<spic::Scene> MainMenuBuilder::BuildScene() {
     background->AddComponent(sprite);
     sprite->SetSprite("assets/title.jpg");
 
-    transfrom.position.x = 600;
-    transfrom.position.y = 40;
+    transfrom.position.x = (screenWidth / 3.1);
+    transfrom.position.y = ((screenHeight - screenHeight) + 50);
     transfrom.scale = 1.5;
 
     background->setTransform(&transfrom);
 
 
-    buttonPlay = std::make_shared<spic::Button>("playbutton", 840, 288, 270, 108, "StartGameScript");
+    buttonPlay = std::make_shared<spic::Button>("playbutton", (screenWidth / 2.3), ((screenHeight - screenHeight) + 288), 270, 108, "StartGameScript");
     buttonPlay->SetName("StartGameButton");
     scene->AddGameObject(buttonPlay);
     std::shared_ptr<ChangeSceneBehaviour> scriptPlay = std::make_shared<ChangeSceneBehaviour>("StartGameScript", "level1");
     buttonPlay->AddComponent(scriptPlay);
 
-    buttonHelp = std::make_shared<spic::Button>("helpbutton", 840, 413, 270, 108, "OpenHelpScript");
+    buttonHelp = std::make_shared<spic::Button>("helpbutton", (screenWidth / 2.3), ((screenHeight - screenHeight) + 413), 270, 108, "OpenHelpScript");
     buttonHelp->SetName("HelpButton");
     scene->AddGameObject(buttonHelp);
     std::shared_ptr<ChangeSceneBehaviour> scriptHelp = std::make_shared<ChangeSceneBehaviour>("OpenHelpScript", "HelpScene");
     buttonHelp->AddComponent(scriptHelp);
 
-    buttonCredits = std::make_shared<spic::Button>("creditsbutton", 840, 538, 270, 108, "OpenCreditsScript");
+    buttonCredits = std::make_shared<spic::Button>("creditsbutton", (screenWidth / 2.3), ((screenHeight - screenHeight) + 538), 270, 108, "OpenCreditsScript");
     buttonCredits->SetName("CreditsButton");
     scene->AddGameObject(buttonCredits);
     std::shared_ptr<ChangeSceneBehaviour> scriptCredits = std::make_shared<ChangeSceneBehaviour>("OpenCreditsScript", "CreditsScene");
     buttonCredits->AddComponent(scriptCredits);
 
-    buttonQuit = std::make_shared<spic::Button>("quitbutton", 840, 663, 270, 108, "QuitScript");
+    buttonQuit = std::make_shared<spic::Button>("quitbutton", (screenWidth / 2.3), ((screenHeight - screenHeight) + 663), 270, 108, "QuitScript");
     buttonQuit->SetName("QuitButton");
     scene->AddGameObject(buttonQuit);
     std::shared_ptr<QuitBehaviour> quitScript = std::make_shared<QuitBehaviour>();
