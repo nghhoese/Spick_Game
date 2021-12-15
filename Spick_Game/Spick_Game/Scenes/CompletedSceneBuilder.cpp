73bf68e4b6452d3d5d150ad1e5c71b705bbcb2b4
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-12-15 16:44:03 +0100

    end screen schaalbaar

--- Spick_Game/Spick_Game/Scenes/CompletedSceneBuilder.cpp
+++ Spick_Game/Spick_Game/Scenes/CompletedSceneBuilder.cpp
@@ -4,13 +4,16 @@ std::shared_ptr<spic::Scene> CompletedSceneBuilder::BuildScene()
 {
     std::shared_ptr<spic::Scene> scene = std::make_shared<spic::Scene>("CompletedScene");
 
-    buttonBackToMenu = std::make_shared<spic::Button>("mainmenubutton", 770, 670, 367, 105, "MainMenuScript");
+    int screenWidth = EngineController::GetInstance()->GetScreenWidth();
+    int screenHeight = EngineController::GetInstance()->GetScreenHeight();
+
+    buttonBackToMenu = std::make_shared<spic::Button>("mainmenubutton", (screenWidth / 2.5), (screenHeight - 500), 367, 105, "MainMenuScript");
 
     buttonBackToMenu->SetName("BackToMenuScript");
     scene->AddGameObject(buttonBackToMenu);
 
     spic::Color textColor = spic::Color(1.0, 1.0, 1.0, 1.0);
-    std::shared_ptr<spic::Text> text = std::make_shared<spic::Text>("MISSION COMPLETE", "Capsmall", 140, textColor, 600, 50);
+    std::shared_ptr<spic::Text> text = std::make_shared<spic::Text>("MISSION COMPLETE", "Capsmall", 140, textColor, ((screenWidth / 6)), ((screenHeight - screenHeight) + 100));
 
 
     scene->AddGameObject(text);
